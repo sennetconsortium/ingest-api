c46c8576352f0269fd09eeac186d62eb03ee0636
Karl Burke <kburke@pitt.edu>
2022-09-07 11:06:23 -0400

    Initial datasets POST endpoint creation

--- src/app.py
+++ src/app.py
@@ -15,6 +15,9 @@ from globus_sdk import AccessTokenAuthorizer, AuthClient, ConfidentialAppAuthCli
 # HuBMAP commons
 from hubmap_commons.hm_auth import AuthHelper
 
+# Local modules
+from ingest_file_helper import IngestFileHelper
+
 # Set logging format and level (default is warning)
 # All the API logging is forwarded to the uWSGI server and gets written into the log file `uwsgi-ingest-api.log`
 # Log rotation is handled via logrotate on the host system with a configuration file
@@ -193,8 +196,45 @@ def logout():
 
 @app.route('/datasets', methods=['POST'])
 def create_dataset():
-    return "Placeholder"
+    from hubmap_commons.exceptions import HTTPException
+    from hubmap_commons import file_helper as commons_file_helper
 
+    if not request.is_json:
+        return Response("json request required", 400)
+    try:
+        dataset_request = request.json
+        # auth_helper = AuthHelper.create(app.config['APP_CLIENT_ID'], app.config['APP_CLIENT_SECRET'], globus_groups) # AuthHelper.configured_instance(app.config['APP_CLIENT_ID'], app.config['APP_CLIENT_SECRET'])
+        auth_tokens = auth_helper_instance.getAuthorizationTokens(request.headers)
+        if isinstance(auth_tokens, Response):
+            return(auth_tokens)
+        elif isinstance(auth_tokens, str):
+            token = auth_tokens
+        elif 'nexus_token' in auth_tokens:
+            token = auth_tokens['nexus_token']
+        else:
+            return(Response("Valid nexus auth token required", 401))
+
+        requested_group_uuid = None
+        if 'group_uuid' in dataset_request:
+            requested_group_uuid = dataset_request['group_uuid']
+
+        ingest_helper = IngestFileHelper(app.config)
+        requested_group_uuid = auth_helper_instance.get_write_group_uuid(token, requested_group_uuid)
+        dataset_request['group_uuid'] = requested_group_uuid
+        post_url = commons_file_helper.ensureTrailingSlashURL(app.config['ENTITY_WEBSERVICE_URL']) + 'entities/dataset'
+        response = requests.post(post_url, json = dataset_request, headers = {'Authorization': 'Bearer ' + token, 'X-SenNet-Application':'ingest-api' }, verify = False)
+        if response.status_code != 200:
+            return Response(response.text, response.status_code)
+        new_dataset = response.json()
+
+        ingest_helper.create_dataset_directory(new_dataset, requested_group_uuid, new_dataset['uuid'])
+
+        return jsonify(new_dataset)
+    except HTTPException as hte:
+        return Response(hte.get_description(), hte.get_status_code())
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        return Response("Unexpected error while creating a dataset: " + str(e) + "  Check the logs", 500)
 
 ####################################################################################################
 ## Internal Functions
