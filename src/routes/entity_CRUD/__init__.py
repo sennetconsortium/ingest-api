af897fb685b58baa52eabbd42ca227f71095e119
Karl Burke <kburke@pitt.edu>
2022-09-09 11:13:05 -0400

    Restore missing app.cfg.example settings.  Make changes from Joe reviewing code on a former PR.

--- src/routes/entity_CRUD/__init__.py
+++ src/routes/entity_CRUD/__init__.py
@@ -19,16 +19,15 @@ def create_dataset():
         return Response("json request required", 400)
     try:
         dataset_request = request.json
+        # Get the single Globus groups token for authorization
         auth_helper_instance = AuthHelper.instance()
-        auth_tokens = auth_helper_instance.getAuthorizationTokens(request.headers)
-        if isinstance(auth_tokens, Response):
-            return(auth_tokens)
-        elif isinstance(auth_tokens, str):
-            token = auth_tokens
-        elif 'nexus_token' in auth_tokens:
-            token = auth_tokens['nexus_token']
+        auth_token = auth_helper_instance.getAuthorizationTokens(request.headers)
+        if isinstance(auth_token, Response):
+            return(auth_token)
+        elif isinstance(auth_token, str):
+            token = auth_token
         else:
-            return(Response("Valid nexus auth token required", 401))
+            return Response("Valid Globus groups token required", 401)
 
         requested_group_uuid = None
         if 'group_uuid' in dataset_request:
