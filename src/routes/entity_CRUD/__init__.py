16857b18e209f4c719fa7ca92d3040559852531f
Lisa-Ann B <lisabruney@pitt.edu>
2023-03-24 12:52:41 -0400

    Use ontology where applicable

--- src/routes/entity_CRUD/__init__.py
+++ src/routes/entity_CRUD/__init__.py
@@ -14,6 +14,7 @@ from hubmap_commons.hm_auth import AuthHelper
 from hubmap_commons.exceptions import HTTPException
 from hubmap_commons import file_helper as commons_file_helper
 from atlas_consortia_commons.rest import *
+from atlas_consortia_commons.string import equals
 
 from lib.file_upload_helper import UploadFileHelper
 
@@ -26,7 +27,7 @@ from routes.entity_CRUD.ingest_file_helper import IngestFileHelper
 from routes.entity_CRUD.dataset_helper import DatasetHelper
 from routes.entity_CRUD.constraints_helper import *
 from routes.auth import get_auth_header
-from lib.ontology import Ontology
+from lib.ontology import Ontology, enum_val_lower
 from lib.file import get_csv_records, get_base_path, check_upload
 
 
@@ -514,7 +515,7 @@ def _common_ln_errs(err, val):
 def validate_sources(headers, records):
     error_msg = []
     file_is_valid = True
-    allowed_source_types = ["human", "human organoid", "mouse", "mouse organoid"]
+    allowed_source_types = Ontology.source_types(True, enum_val_lower)
 
     required_headers = ['lab_id', 'source_type', 'selection_protocol', 'lab_notes']
     for field in required_headers:
@@ -596,7 +597,10 @@ def validate_samples(headers, records, header):
             file_is_valid = False
             error_msg.append(_common_ln_errs(2, field))
 
-    allowed_categories = ["block", "section", "suspension", "organ"]
+    allowed_categories = Ontology.specimen_categories(True, enum_val_lower)
+    # Get the ontology classes
+    SpecimenCategories = Ontology.specimen_categories()
+    Entities = Ontology.entities()
 
     with urllib.request.urlopen('https://raw.githubusercontent.com/sennetconsortium/search-api/main/src/search-schema/data/definitions/enums/organ_types.yaml') as urlfile:
         organ_resource_file = yaml.load(urlfile, Loader=yaml.FullLoader)
@@ -660,11 +664,11 @@ def validate_samples(headers, records, header):
 
             # validate organ_type
             organ_type = data_row['organ_type']
-            if sample_category.lower() != "organ":
+            if not equals(sample_category, SpecimenCategories.ORGAN):
                 if len(organ_type) > 0:
                     file_is_valid = False
                     error_msg.append(_ln_err("field must be blank if `sample_category` is not `organ`", rownum, "organ_type"))
-            if sample_category.lower() == "organ":
+            if equals(sample_category, SpecimenCategories.ORGAN):
                 if len(organ_type) < 1:
                     file_is_valid = False
                     error_msg.append(_ln_err("field is required if `sample_category` is `organ`", rownum, "organ_type"))
@@ -682,11 +686,11 @@ def validate_samples(headers, records, header):
 
             if ancestor_saved or resp_status_code:
                 data_row['ancestor_id'] = ancestor_dict['uuid']
-                if sample_category.lower() == 'organ' and ancestor_dict['type'].lower() != 'source':
+                if equals(sample_category, SpecimenCategories.ORGAN) and not equals(ancestor_dict['type'], Entities.SOURCE):
                     file_is_valid = False
                     error_msg.append(_ln_err("If `sample_category` is `organ`, `ancestor_id` must point to a source", rownum))
 
-                if sample_category.lower() != 'organ' and ancestor_dict['type'].lower() != 'sample':
+                if not equals(sample_category, SpecimenCategories.ORGAN) and not equals(ancestor_dict['type'], Entities.SAMPLE):
                     file_is_valid = False
                     error_msg.append(_ln_err("If `sample_category` is not `organ`, `ancestor_id` must point to a sample", rownum))
 
@@ -695,10 +699,10 @@ def validate_samples(headers, records, header):
                 sub_type_val = None
                 if valid_category:
                     sub_type = get_as_list(sample_category)
-                if sample_category.lower() == "organ":
+                if equals(sample_category, SpecimenCategories.ORGAN):
                     sub_type_val = get_as_list(organ_type)
 
-                entity_to_validate = build_constraint_unit('Sample', sub_type, sub_type_val)
+                entity_to_validate = build_constraint_unit(Entities.SAMPLE, sub_type, sub_type_val)
                 try:
                     entity_constraint_list = append_constraints_list(entity_to_validate, ancestor_dict, header, entity_constraint_list, ancestor_id)
 
@@ -821,7 +825,7 @@ def validate_datasets(headers, records, header):
                     if data_types_valid:
                         sub_type = get_as_list(data_types)
 
-                    entity_to_validate = build_constraint_unit('Dataset', sub_type)
+                    entity_to_validate = build_constraint_unit(Ontology.entities().DATASET, sub_type)
 
                     try:
                         entity_constraint_list = append_constraints_list(entity_to_validate, ancestor_dict, header, entity_constraint_list, ancestor_id)
@@ -881,19 +885,19 @@ def validate_ancestor_id(header, ancestor_id, error_msg, rownum, valid_ancestor_
 
 
 def append_constraints_list(entity_to_validate, ancestor_dict, header, entity_constraint_list, ancestor_id):
-
+    Entities = Ontology.entities()
     ancestor_entity_type = ancestor_dict['type'].lower()
     url = commons_file_helper.ensureTrailingSlashURL(current_app.config['ENTITY_WEBSERVICE_URL']) + 'entities/' + ancestor_id
 
     ancestor_result = requests.get(url, headers=header).json()
     sub_type = None
     sub_type_val = None
-    if ancestor_entity_type == "dataset":
+    if equals(ancestor_entity_type, Entities.DATASET):
         sub_type = get_as_list(ancestor_result['data_types'])
 
-    if ancestor_entity_type == "sample":
+    if equals(ancestor_entity_type, Entities.SAMPLE):
         sub_type = get_as_list(ancestor_result['sample_category'])
-        if ancestor_result['sample_category'] == 'organ':
+        if equals(ancestor_result['sample_category'], Ontology.specimen_categories().ORGAN):
             sub_type_val = get_as_list(ancestor_result['organ'])
 
     ancestor_to_validate = build_constraint_unit(ancestor_entity_type, sub_type, sub_type_val)
