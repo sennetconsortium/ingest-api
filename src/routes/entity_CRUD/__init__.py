152ed80e1288772b6f5e9adb0e8901f8510cc64e
maxsibilla <maxsibilla@icloud.com>
2023-10-17 14:21:30 -0400

    Updating symlink call for multiple components

--- src/routes/entity_CRUD/__init__.py
+++ src/routes/entity_CRUD/__init__.py
@@ -123,12 +123,12 @@ def multiple_components():
 
         for dataset in new_datasets_list:
             # The property `dataset_link_abs_dir` will contain the filepath to the existing directory located inside the primary multi-assay
-            # directory. We need to create a new directory for each secondary dataset and then create a symlink to the aforementioned directory
+            # directory. We need to create a symlink to the aforementioned directory at the path for the newly created datsets.
             if 'dataset_link_abs_dir' in dataset:
                 new_directory_path = ingest_helper.get_dataset_directory_absolute_path(dataset, requested_group_uuid, dataset['uuid'])
                 logger.info(
                     f"Creating a directory as: {new_directory_path} with a symbolic link to: {dataset['dataset_link_abs_dir']}")
-                ingest_helper.make_directory(new_directory_path, dataset['dataset_link_abs_dir'])
+                os.symlink(dataset['dataset_link_abs_dir'], new_directory_path, True)
             else:
                 return Response("Required field 'dataset_link_abs_dir' is missing from dataset", 500)
 
