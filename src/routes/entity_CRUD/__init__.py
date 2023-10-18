2e1c54dcec76d679db9fd12b053fa66c1cabda69
maxsibilla <maxsibilla@icloud.com>
2023-10-18 17:40:58 -0400

    Creating file directory before creating symlink

--- src/routes/entity_CRUD/__init__.py
+++ src/routes/entity_CRUD/__init__.py
@@ -129,8 +129,8 @@ def multiple_components():
                 new_directory_path = ingest_helper.get_dataset_directory_absolute_path(dataset, requested_group_uuid, dataset['uuid'])
                 logger.info(
                     f"Creating a directory as: {new_directory_path} with a symbolic link to: {dataset['dataset_link_abs_dir']}")
-                # os.symlink(dataset['dataset_link_abs_dir'], new_directory_path, True)
-                ingest_helper.make_directory(new_directory_path, dataset['dataset_link_abs_dir'])
+                os.makedirs(new_directory_path)
+                os.symlink(dataset['dataset_link_abs_dir'], new_directory_path, True)
             else:
                 return Response("Required field 'dataset_link_abs_dir' is missing from dataset", 500)
 
