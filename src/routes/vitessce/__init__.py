62949ca20daf3ab3c59176bcaa54fd6b2c90f49c
Tyler Madonna <tjm159@pitt.edu>
2024-01-09 16:39:59 -0500

    Adding validation and error handling to vitessce

--- src/routes/vitessce/__init__.py
+++ src/routes/vitessce/__init__.py
@@ -1,6 +1,7 @@
 import logging
+from uuid import UUID
 
-from flask import Blueprint, Response, current_app, jsonify, request
+from flask import Blueprint, current_app, jsonify, request
 from hubmap_commons.hm_auth import AuthHelper
 from portal_visualization.builder_factory import get_view_config_builder
 
@@ -14,6 +15,9 @@ logger = logging.getLogger(__name__)
 @vitessce_blueprint.route("/vitessce/config", methods=["POST"])
 @require_json(param="entity")
 def vitessce_config(entity: dict):
+    if errors := validate_vitessce_entity(entity) :
+        return jsonify({"error": errors}), 400
+
     def get_assaytype(entity):
         if "metadata" in entity.get("metadata", {}):
             metadata = entity["metadata"]["metadata"]
@@ -38,7 +42,43 @@ def vitessce_config(entity: dict):
             entity, groups_token, current_app.config["ASSETS_WEBSERVICE_URL"]
         )
         vitessce_conf = builder.get_conf_cells(marker=None)
-        return jsonify(vitessce_conf[0] or {}), 200
+        if len(vitessce_conf) < 1 or not vitessce_conf[0]:
+            raise ValueError("empty vitessce config")
+        return jsonify(vitessce_conf[0]), 200
     except Exception as e:
-        logger.error(e)
-        return Response("Unexpected error while retrieving Vitessce config", 500)
+        logger.error(f"Error while retrieving Vitessce config for uuid {entity['uuid']}:", e)
+        return jsonify({"error": "404 Not Found: Entity or filepath not found"}), 404
+
+
+def validate_vitessce_entity(entity):
+    """Add basic validation for vitessce config request body
+
+    Args:
+        entity (dict): vitessce config request body
+
+    Returns:
+        list: list of errors
+    """
+    errors = []
+    try:
+        UUID(entity.get("uuid"))
+    except Exception:
+        errors.append("uuid must be a valid UUID")
+
+    if not entity.get("status") or not isinstance(entity["status"], str):
+        errors.append("'status' string is required")
+
+    if not entity.get("data_types") or not isinstance(entity["data_types"], list):
+        errors.append("'data_types' array is required")
+
+    if not entity.get("files") or not isinstance(entity["files"], list):
+        errors.append("'files' array is required")
+
+    if entity.get("entity_type") != "Dataset":
+        errors.append("'entity_type' 'Dataset' is required")
+
+    prov_list = entity.get("metadata", {}).get("dag_provenance_list")
+    if not prov_list or not isinstance(prov_list, list):
+        errors.append("'metadata.dag_provenance_list' array is required")
+
+    return errors
