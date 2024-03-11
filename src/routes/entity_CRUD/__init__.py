48532af5c9b56d855e133999e943fd6d6f62a41d
Tyler Madonna <tjm159@pitt.edu>
2024-03-11 12:33:37 -0400

    Moving try block in bulk dataset submission

--- src/routes/entity_CRUD/__init__.py
+++ src/routes/entity_CRUD/__init__.py
@@ -315,34 +315,35 @@ def create_datasets_from_bulk():
 @require_json(param='uuids')
 def submit_datasets_from_bulk(uuids: list, token: str):
     if not isinstance(uuids, list) or len(uuids) == 0:
-        abort_bad_req("A list of dataset uuids is required")
+        abort_bad_req('A list of dataset uuids is required')
 
     dataset_helper = DatasetHelper(current_app.config)
     uuids = set(uuids)
     try:
         fields = {'uuid'}
         datasets = dataset_helper.get_datasets_by_uuid(uuids, fields)
-        if datasets is None:
-            abort_not_found('No datasets found with any of the provided uuids')
-
-        diff = uuids.difference({dataset['uuid'] for dataset in datasets})
-        if len(diff) > 0:
-            abort_not_found(f"No datasets found with the following uuids: {', '.join(diff)}")
-
-        try:
-            Thread(target=submit_datasets, args=[uuids, token, current_app.config]).start()
-            logger.info(
-                f"Started to submit datasetsa for processing with uuids: {uuids}"
-            )
-        except Exception as e:
-            logger.exception(e)
-            abort_internal_err(e)
-
-        # return a 202 reponse with the accepted dataset uuids
-        return jsonify(list(uuids)), 202
     except Exception as e:
         logger.error(f'Error while submitting datasets: {str(e)}')
-        abort_internal_err('Error while submitting datasets')
+        abort_internal_err(str(e))
+
+    if datasets is None:
+        abort_not_found('No datasets found with any of the provided uuids')
+
+    diff = uuids.difference({dataset['uuid'] for dataset in datasets})
+    if len(diff) > 0:
+        abort_not_found(f"No datasets found with the following uuids: {', '.join(diff)}")
+
+    try:
+        Thread(target=submit_datasets, args=[uuids, token, current_app.config]).start()
+        logger.info(
+            f'Started to submit datasetsa for processing with uuids: {uuids}'
+        )
+    except Exception as e:
+        logger.error(f'Error while submitting datasets: {str(e)}')
+        abort_internal_err(str(e))
+
+    # return a 202 reponse with the accepted dataset uuids
+    return jsonify(list(uuids)), 202
 
 
 @entity_CRUD_blueprint.route('/uploads/<ds_uuid>/file-system-abs-path', methods=['GET'])
