ff0c11aab878e5c944097a2973eb6832f6b25381
Tyler Madonna <tjm159@pitt.edu>
2024-03-18 17:50:37 -0400

    Adding get tasks endpoint

--- src/routes/tasks/__init__.py
+++ src/routes/tasks/__init__.py
@@ -1,9 +1,8 @@
 import logging
-from typing import Union
 from uuid import UUID
 
 from atlas_consortia_commons.rest import abort_internal_err, abort_not_found
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from rq.job import Job, JobStatus, NoSuchJobError
 
 from lib.decorators import require_data_admin, require_valid_token
@@ -28,21 +27,28 @@ def get_task(task_id: UUID, user_id: str):
         logger.error(f"Task not found: {e}")
         abort_not_found("Task not found")
 
-    return job_to_response(job, task_id), 200
+    return job_to_response(job), 200
 
-    return {
-        "task_id": task_id,
-        "description": job.meta.get("description"),
-        "status": status.title(),
-        "started_timestamp": (
-            int(job.started_at.timestamp() * 1000) if job.started_at else None
-        ),
-        "ended_timestamp": (
-            int(job.ended_at.timestamp() * 1000) if job.ended_at else None
-        ),
-        "results": results,
-        "errors": errors,
-    }, 200
+
+@tasks_blueprint.route("/tasks", methods=["GET"])
+@require_valid_token(user_id_param="user_id")
+def get_tasks(user_id: str):
+    if TaskQueue.is_initialized() is False:
+        logger.error("Task queue has not been initialized")
+        abort_internal_err("Unable to retrieve task information")
+
+    task_queue = TaskQueue.instance()
+    redis = task_queue.redis
+
+    # this returns a list of byte objects
+    prefix = "rq:job:"
+    queue_ids = [
+        queue_id.decode("utf-8").removeprefix(prefix)
+        for queue_id in redis.scan_iter(f"{prefix}{user_id}:*")
+    ]
+    jobs = Job.fetch_many(queue_ids, connection=redis)
+    res = [job_to_response(job) for job in jobs]
+    return jsonify(res), 200
 
 
 @tasks_blueprint.route("/tasks/flush", methods=["DELETE"])
@@ -65,7 +71,8 @@ def flush_tasks():
     return {"status": "success", "message": "All tasks have been deleted"}, 200
 
 
-def job_to_response(job: Job, task_id: Union[str, UUID]) -> dict:
+def job_to_response(job: Job) -> dict:
+    _, task_id = TaskQueue.instance().split_queue_id(job.id)
     status = job.get_status()
     results = None
     errors = None
