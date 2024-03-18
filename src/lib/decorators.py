6a01fdb285938439df78020fbc4f47aa33ba1dc7
Tyler Madonna <tjm159@pitt.edu>
2024-03-18 12:47:40 -0400

    Merging flask route decorators

--- src/lib/decorators.py
+++ src/lib/decorators.py
@@ -2,22 +2,44 @@ import contextlib
 import os
 from functools import wraps
 from inspect import signature
+from typing import Optional
 
-from atlas_consortia_commons.rest import abort_bad_req
-from flask import request
+from atlas_consortia_commons.rest import (
+    abort_bad_req,
+    abort_forbidden,
+    abort_internal_err,
+    abort_unauthorized,
+)
+from flask import current_app, request
+from hubmap_commons.hm_auth import AuthHelper
 
 
-def require_json(param="body"):
-    """A decorator that checks if the request content type is json. If the decorated function
-    has a parameter named `param`, the request body will be passed as that parameter
+def require_json(param: str = "body"):
+    """A decorator that checks if the request content type is json.
 
-    Args:
-        param (str, optional): The name of the parameter to pass the request body to. Defaults to "body".
+    If the decorated function has a parameter with the same name as `param`, the
+    request body will be passed as that parameter.
 
-    Example:
+    Parameters
+    ----------
+    param : str
+        The name of the parameter to pass the request body to required.
+        Defaults to "body".
+
+    Notes
+    -----
+    This decorator does not do any validation on the json request body.
+
+    Example
+    -------
         @app.route("/foo", methods=["POST"])
-        @require_json(param="body")
-        def foo(body: dict):
+        @require_json(param="foo_body")
+        def foo(foo_body: dict):
+            return jsonify(foo_body)
+
+        @app.route("/bar", methods=["PUT"])
+        @require_json()
+        def bar(body: dict):
             return jsonify(body)
     """
 
@@ -29,8 +51,138 @@ def require_json(param="body"):
                     "A json body and appropriate Content-Type header are required"
                 )
 
-            if param in signature(f).parameters:
+            if param and param in signature(f).parameters:
                 kwargs[param] = request.json
+
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
+
+
+def require_data_admin(param: str = "token"):
+    """A decorator that checks if the user is a member of the SenNet Data Admin group.
+
+    If the decorated function has a parameter with the same name as `param`, the
+    user's token will be passed as that parameter. If the request has no token or an
+    invalid token, a 401 Unauthorized response will be returned. If the user is not a
+    member of the SenNet Data Admin group, a 403 Forbidden response will be returned.
+
+    Parameters
+    ----------
+    param : str
+        The name of the parameter to pass the user's token to. Defaults to "token".
+
+    Example
+    -------
+        @app.route("/foo", methods=["POST"])
+        @require_data_admin(param="foo_token")
+        def foo(foo_token: str):
+            return jsonify({"message": f"You are a data admin with token {foo_token}!"})
+
+        @app.route("/bar", methods=["PUT"])
+        @require_data_admin()
+        def bar(token: str):
+            return jsonify({"message": f"You are a data admin with token {token}!"})
+    """
+
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            auth_helper = AuthHelper.configured_instance(
+                current_app.config["APP_CLIENT_ID"],
+                current_app.config["APP_CLIENT_SECRET"],
+            )
+            token = auth_helper.getUserTokenFromRequest(request, getGroups=True)
+            if not isinstance(token, str):
+                abort_unauthorized("User must be a member of the SenNet Consortium")
+
+            is_data_admin = auth_helper.has_data_admin_privs(token)
+            if is_data_admin is not True:
+                abort_forbidden("User must be a member of the SenNet Data Admin group")
+
+            if param and param in signature(f).parameters:
+                kwargs[param] = token
+
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
+
+
+def require_valid_token(
+    param: str = "token",
+    user_id_param: Optional[str] = None,
+    groups_param: Optional[str] = None,
+):
+    """A decorator that checks if the provided token is valid.
+
+    If the decorated function has a parameter with the same name as `param`, the
+    user's token will be passed as that parameter. If the request has no token or an
+    invalid token, a 401 Unauthorized response will be returned.
+
+    If the decorated function has a parameter with the same name as `user_id_param`, the
+    user's id will be passed as that parameter.
+
+    If the decorated function has a parameter with the same name as `groups_param`, the
+    user's group ids will be passed as that parameter.
+
+    Parameters
+    ----------
+    param : str
+        The name of the parameter to pass the user's token to. Defaults to "token".
+    user_id_param : Optional[str]
+        The name of the parameter to pass the user's id to. Defaults to None.
+    groups_param : Optional[str]
+        The name of the parameter to pass the user's group ids to. Defaults to None.
+
+    Example
+    -------
+        @app.route("/foo", methods=["POST"])
+        @require_valid_token(param="foo_token", groups_param="foo_groups")
+        def foo(foo_token: str, foo_groups: list):
+            return jsonify({
+                "message": (
+                    f"You are a valid user with token {foo_token} "
+                    f"and groups {foo_groups}!"
+                )
+            })
+
+        @app.route("/bar", methods=["PUT"])
+        @require_valid_token()
+        def bar(token: str):
+            return jsonify({"message": f"You are a valid user with token {token}!"})
+    """
+
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            auth_helper = AuthHelper.configured_instance(
+                current_app.config["APP_CLIENT_ID"],
+                current_app.config["APP_CLIENT_SECRET"],
+            )
+
+            token = auth_helper.getUserTokenFromRequest(request, getGroups=True)
+            if not isinstance(token, str):
+                abort_unauthorized("User must be a member of the SenNet Consortium")
+
+            user_info = auth_helper.getUserInfo(token, getGroups=True)
+            if not isinstance(user_info, dict):
+                abort_unauthorized("User must be a member of the SenNet Consortium")
+
+            if param in signature(f).parameters:
+                kwargs[param] = token
+
+            if user_id_param and user_id_param in signature(f).parameters:
+                user_id = user_info.get("sub")
+                if not user_id:
+                    abort_internal_err("User id not found for token")
+                kwargs[user_id_param] = user_id
+
+            if groups_param and groups_param in signature(f).parameters:
+                kwargs[groups_param] = user_info.get("hmgroupids", [])
 
             return f(*args, **kwargs)
 
