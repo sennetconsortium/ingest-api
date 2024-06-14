e4331ae5965aa34256505bf4b9ee680f0535d3c8
Lisa-Ann B <lisabruney@pitt.edu>
2024-06-14 16:17:44 -0400

    Update logic to satisfy: first parameter sets where the symlink will exist and it needs to be in the updated location -#479

--- src/lib/ingest_file_helper.py
+++ src/lib/ingest_file_helper.py
@@ -188,9 +188,7 @@ class IngestFileHelper:
         to_path = self.dataset_directory_absolute_path(data_access_level, group_uuid, uuid, True)
         if not trial_run:
             if to_symlink_path is not None:
-                shutil.move(from_path, to_path)
                 os.symlink(to_path, to_symlink_path, True)
-                # or is it? os.symlink(from_path, to_symlink_path, True)
             else:
                 shutil.move(from_path, to_path)
         else:
