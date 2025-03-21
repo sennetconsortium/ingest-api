33f1cb4308509b4ddfbf29e6d960b2bf902a008d
maxsibilla <maxsibilla@icloud.com>
2025-03-21 16:13:25 -0400

    Porting over rule chain class updates

--- src/lib/rule_chain.py
+++ src/lib/rule_chain.py
@@ -231,41 +231,46 @@ class RuleSyntaxException(Exception):
 
 
 class RuleLoader:
-    def __init__(self, stream, format="yaml"):
+    def __init__(self, stream, format='yaml'):
         self.stream = stream
-        assert format in ["yaml", "json"], f"unknown format {format}"
+        assert format in ['yaml', 'json'], f"unknown format {format}"
         self.format = format
-
     def load(self):
-        rule_chain = RuleChain()
-        if self.format == "yaml":
-            json_recs = yaml.safe_load(self.stream)
-        elif self.format == "json":
+        rule_chain_dict = {}
+        if self.format == 'yaml':
+            json_dict = yaml.safe_load(self.stream)
+        elif self.format == 'json':
             if isinstance(self.stream, str):
-                json_recs = json.loads(self.stream)
+                json_dict = json.loads(self.stream)
             else:
-                json_recs = json.load(self.stream)
+                json_dict = json.load(self.stream)
         else:
             raise RuntimeError(f"Unknown format {self.format} for input stream")
-        check_json_matches_schema(
-            json_recs, SCHEMA_FILE, str(Path(__file__).parent), SCHEMA_BASE_URI
-        )
-        for rec in json_recs:
-            for rule in [rec[key] for key in ["match", "value"]]:
-                assert Rule.is_valid(rule), f"Syntax error in rule string {rule}"
-            try:
-                rule_cls = {"note": NoteRule, "match": MatchRule}[rec["type"].lower()]
-            except KeyError:
-                raise RuleSyntaxException(f"Unknown rule type {rec['type']}")
-            rule_chain.add(rule_cls(rec["match"], rec["value"]))
-        return rule_chain
+        check_json_matches_schema(json_dict,
+                                  SCHEMA_FILE,
+                                  str(Path(__file__).parent),
+                                  SCHEMA_BASE_URI)
+        for key in json_dict:
+            rule_chain = RuleChain()
+            json_recs = json_dict[key]
+            for rec in json_recs:
+                for rule in [rec[key2] for key2 in ['match', 'value']]:
+                    assert Rule.is_valid(rule), f"Syntax error in rule string {rule}"
+                try:
+                    rule_cls = {'note': NoteRule,
+                                'match': MatchRule}[rec['type'].lower()]
+                    rule_chain.add(rule_cls(rec['match'], rec['value']))
+                except KeyError:
+                    raise RuleSyntaxException(f"Unknown rule type {rec['type']}")
+            rule_chain_dict[key] = rule_chain
+        return rule_chain_dict
+
 
 
 class _RuleChainIter:
     def __init__(self, rule_chain):
         self.offset = 0
         self.rule_chain = rule_chain
-
     def __next__(self):
         if self.offset < len(self.rule_chain.links):
             rslt = self.rule_chain.links[self.offset]
@@ -273,7 +278,6 @@ class _RuleChainIter:
             return rslt
         else:
             raise StopIteration
-
     def __iter__(self):
         return self
 
@@ -281,50 +285,48 @@ class _RuleChainIter:
 class RuleChain:
     def __init__(self):
         self.links = []
-
     def add(self, link):
         self.links.append(link)
-
     def dump(self, ofile):
-        print(f"START DUMP of {len(list(iter(self)))} rules")
+        ofile.write(f"START DUMP of {len(list(iter(self)))} rules\n")
         for idx, elt in enumerate(iter(self)):
-            print(f"{idx}: {elt}")
-        print("END DUMP of rules")
-
+            ofile.write(f"{idx}: {elt}\n")
+        ofile.write(f"END DUMP of rules\n")
     def __iter__(self):
         return _RuleChainIter(self)
-
     @classmethod
     def cleanup(cls, val):
         """
         Convert val to JSON-appropriate data types
         """
-        if isinstance(val, dict):  # includes OrderedDict
+        if isinstance(val, dict): # includes OrderedDict
             return dict({cls.cleanup(key): cls.cleanup(val[key]) for key in val})
         elif isinstance(val, list):
             return list(cls.cleanup(elt) for elt in val)
         else:
             return val
-
-    def apply(self, rec):
-        ctx = {}  # so rules can leave notes for later rules
+    def apply(self, rec, ctx = None):
+        if ctx is None:
+            ctx = {}  # so rules can leave notes for later rules
         for elt in iter(self):
-            rec_dict = rec | ctx
+            if ctx.get("DEBUG"):
+                logger.debug(f"applying {elt} to rec:{rec}  ctx:{ctx}")
+            rec_dict = rec | ctx;
             try:
                 if elt.match_rule.matches(rec_dict):
                     val = elt.val_rule.evaluate(rec_dict)
                     if isinstance(elt, MatchRule):
                         return self.cleanup(val)
                     elif isinstance(elt, NoteRule):
-                        assert isinstance(
-                            val, dict
-                        ), f"Rule {elt} applied to {rec_dict} did not produce a dict"
+                        assert isinstance(val, dict), f"Rule {elt} applied to {rec_dict} did not produce a dict"
                         ctx.update(val)
                     else:
                         raise NotImplementedError(f"Unknown rule type {type(elt)}")
             except EngineError as excp:
-                print(f"ENGINE_ERROR {type(excp)} {excp}")
+                logger.error(f"ENGINE_ERROR {type(excp)} {excp}")
                 raise RuleLogicException(excp) from excp
+            if ctx.get("DEBUG"):
+                logger.debug("done")
         raise NoMatchException(f"No rule matched record {rec}")
 
 
