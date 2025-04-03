224f124557d06a04466b601ab36934aac7817f35
Tyler Madonna <tjm159@pitt.edu>
2025-04-03 14:40:23 -0400

    Adding logging to and cleaning up assay classifier

--- src/routes/assayclassifier/__init__.py
+++ src/routes/assayclassifier/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import urllib
 
-from atlas_consortia_commons.decorator import require_json, require_valid_token, User
+from atlas_consortia_commons.decorator import require_json, require_valid_token
 from flask import Blueprint, Response, jsonify
 from hubmap_commons.exceptions import HTTPException
 from werkzeug.exceptions import HTTPException as WerkzeugException
@@ -33,31 +33,37 @@ def get_ds_assaytype(ds_uuid: str):
         metadata = build_entity_metadata(entity)
         is_human = source_is_human(
             [ds_uuid],
-            token
+            token,
         )
-        rules_json = calculate_assay_info(metadata,
-                                          is_human,
-                                          get_data_from_ubkg
-                                          )
+        rules_json = calculate_assay_info(
+            metadata,
+            is_human,
+            get_data_from_ubkg,
+        )
         return jsonify(rules_json)
     except ValueError as excp:
         logger.error(excp, exc_info=True)
-        return Response("Bad parameter: {excp}", 400)
+        return Response(f"Bad parameter: {excp}", 400)
     except ResponseException as re:
         logger.error(re, exc_info=True)
         return re.response
     except NoMatchException as excp:
+        logger.error(excp, exc_info=True)
         return {}
     except (RuleSyntaxException, RuleLogicException) as excp:
+        logger.error(excp, exc_info=True)
         return Response(f"Error applying classification rules: {excp}", 500)
     except WerkzeugException as excp:
+        logger.error(excp, exc_info=True)
         return excp
     except HTTPException as hte:
+        logger.error(hte, exc_info=True)
         return Response(
-            f"Error while getting assay type for {ds_uuid}: " + hte.get_description(),
+            f"Error while getting assay type for {ds_uuid}: {hte.get_description()}",
             hte.get_status_code(),
         )
     except urllib.error.HTTPError as hte:
+        logger.error(hte, exc_info=True)
         return Response(
             f"Error while getting assay type for {ds_uuid}: {hte}",
             hte.status,
@@ -65,7 +71,8 @@ def get_ds_assaytype(ds_uuid: str):
     except Exception as e:
         logger.error(e, exc_info=True)
         return Response(
-            f"Unexpected error while retrieving entity {ds_uuid}: " + str(e), 500
+            f"Unexpected error while retrieving entity {ds_uuid}: " + str(e),
+            500,
         )
 
 
@@ -80,22 +87,27 @@ def get_ds_rule_metadata(ds_uuid: str):
         return jsonify(metadata)
     except ValueError as excp:
         logger.error(excp, exc_info=True)
-        return Response("Bad parameter: {excp}", 400)
+        return Response(f"Bad parameter: {excp}", 400)
     except ResponseException as re:
         logger.error(re, exc_info=True)
         return re.response
     except NoMatchException as excp:
+        logger.error(excp, exc_info=True)
         return {}
     except (RuleSyntaxException, RuleLogicException) as excp:
+        logger.error(excp, exc_info=True)
         return Response(f"Error applying classification rules: {excp}", 500)
     except WerkzeugException as excp:
+        logger.error(excp, exc_info=True)
         return excp
     except HTTPException as hte:
+        logger.error(hte, exc_info=True)
         return Response(
-            f"Error while getting assay type for {ds_uuid}: " + hte.get_description(),
+            f"Error while getting assay type for {ds_uuid}: {hte.get_description()}",
             hte.get_status_code(),
         )
     except urllib.error.HTTPError as hte:
+        logger.error(hte, exc_info=True)
         return Response(
             f"Error while getting assay type for {ds_uuid}: {hte}",
             hte.status,
@@ -103,18 +115,18 @@ def get_ds_rule_metadata(ds_uuid: str):
     except Exception as e:
         logger.error(e, exc_info=True)
         return Response(
-            f"Unexpected error while retrieving entity {ds_uuid}: " + str(e), 500
+            f"Unexpected error while retrieving entity {ds_uuid}: {e}",
+            500,
         )
 
 
 @assayclassifier_blueprint.route("/assaytype", methods=["POST"])
 @require_valid_token()
 @require_json(param="metadata")
-def get_assaytype_from_metadata(token: str, user: User, metadata: dict):
+def get_assaytype_from_metadata(token: str, metadata: dict):
     try:
         if parent_sample_ids := metadata.get("parent_sample_id"):
-            is_human = source_is_human(parent_sample_ids.split(","),
-                                       token)
+            is_human = source_is_human(parent_sample_ids.split(","), token)
         else:
             is_human = True  # default to human for safety
         rules_json = calculate_assay_info(metadata, is_human, get_data_from_ubkg)
@@ -123,17 +135,22 @@ def get_assaytype_from_metadata(token: str, user: User, metadata: dict):
         logger.error(re, exc_info=True)
         return re.response
     except NoMatchException as excp:
+        logger.error(excp, exc_info=True)
         return {}
     except (RuleSyntaxException, RuleLogicException) as excp:
+        logger.error(excp, exc_info=True)
         return Response(f"Error applying classification rules: {excp}", 500)
     except WerkzeugException as excp:
+        logger.error(excp, exc_info=True)
         return excp
     except HTTPException as hte:
+        logger.error(hte, exc_info=True)
         return Response(
-            f"Error while getting assay type from metadata: " + hte.get_description(),
+            f"Error while getting assay type from metadata: {hte.get_description()}",
             hte.get_status_code(),
         )
     except urllib.error.HTTPError as hte:
+        logger.error(hte, exc_info=True)
         return Response(
             f"Error while getting assay type from metadata: {hte}",
             hte.status,
@@ -141,7 +158,8 @@ def get_assaytype_from_metadata(token: str, user: User, metadata: dict):
     except Exception as e:
         logger.error(e, exc_info=True)
         return Response(
-            f"Unexpected error while getting assay type from metadata: " + str(e), 500
+            f"Unexpected error while getting assay type from metadata: {e}",
+            500,
         )
 
 
@@ -154,12 +172,14 @@ def reload_chain():
         logger.error(re, exc_info=True)
         return re.response
     except (RuleSyntaxException, RuleLogicException) as excp:
+        logger.error(excp, exc_info=True)
         return Response(f"Error applying classification rules: {excp}", 500)
     except HTTPException as hte:
+        logger.error(hte, exc_info=True)
         return Response(
-            "Error while getting assay types: " + hte.get_description(),
+            f"Error while getting assay types: {hte.get_description()}",
             hte.get_status_code(),
         )
     except Exception as e:
         logger.error(e, exc_info=True)
-        return Response(f"Unexpected error while reloading rule chain: " + str(e), 500)
+        return Response(f"Unexpected error while reloading rule chain: {e}", 500)
