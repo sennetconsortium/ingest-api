16c8ce8466e83d39db89d06a7a57b0245758ca39
maxsibilla <maxsibilla@icloud.com>
2026-05-08 13:54:13 -0400

    Add additional senotype privs endpoints

--- src/routes/privs/__init__.py
+++ src/routes/privs/__init__.py
@@ -4,6 +4,7 @@ from typing import List
 from flask import Blueprint, Response, jsonify, make_response, request, current_app
 from hubmap_commons.exceptions import HTTPException
 from hubmap_commons.hm_auth import AuthHelper
+from rdflib import void
 
 privs_blueprint = Blueprint("privs", __name__)
 logger = logging.getLogger(__name__)
@@ -57,21 +58,15 @@ def privs_get_user_write_groups():
 
 @privs_blueprint.route("/privs/has-senotype-edit")
 def privs_has_senotype_edit():
-    groups_token: str = get_groups_token()
-    auth_helper_instance: AuthHelper = AuthHelper.instance()
-    has_senotype_edit = False
+    return check_groups_access('has_senotype_edit', current_app.config["SENOTYPE_EDIT_UUID"])
 
-    user_info = auth_helper_instance.getUserInfo(groups_token, getGroups=True)
-    if isinstance(user_info, Response):
-        return user_info
+@privs_blueprint.route("/privs/has-senotype-curate")
+def privs_has_senotype_curate():
+    return check_groups_access('has_senotype_curate', current_app.config["SENOTYPE_CURATE_UUID"])
 
-    if current_app.config["SENOTYPE_GROUP_UUID"] in user_info['hmgroupids']:
-        has_senotype_edit = True
-
-    headers: dict = {"Content-Type": "application/json"}
-    return make_response(jsonify({"has_senotype_edit": has_senotype_edit}), 200, headers)
-
-
+@privs_blueprint.route("/privs/has-senotype-publish")
+def privs_has_senotype_publish():
+    return check_groups_access('has_senotype_publish', current_app.config["SENOTYPE_PUBLISH_UUID"])
 
 @privs_blueprint.route("/privs/has-data-admin")
 def privs_has_data_admin_privs():
@@ -92,3 +87,18 @@ def get_groups_token() -> str:
         if request.headers.get("authorization") is not None
         else ""
     )
+
+def check_groups_access(group_name: str, group_uuid: str):
+    groups_token: str = get_groups_token()
+    auth_helper_instance: AuthHelper = AuthHelper.instance()
+    has_group_access = False
+
+    user_info = auth_helper_instance.getUserInfo(groups_token, getGroups=True)
+    if isinstance(user_info, Response):
+        return user_info
+
+    if group_uuid in user_info['hmgroupids']:
+        has_group_access = True
+
+    headers: dict = {"Content-Type": "application/json"}
+    return make_response(jsonify({group_name: has_group_access}), 200, headers)
